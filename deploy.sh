d65c97682d4ff00e432fcb6cf763fa32b33c4071
Caleb Taylor <aquaductape@gmail.com>
2021-08-10 20:38:47 -0700

    update

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,51 @@
-#!/usr/bin/env sh
+#!/bin/bash
+# https://gist.github.com/cobyism/4730490#gistcomment-1374989 current
+# https://gist.github.com/cobyism/4730490#gistcomment-1394421 better but you don't have set up that way currently
 
-# abort on errors
-set -e
+branch="gh-pages"
+output="dist"
 
-# build
-npm run build
+if [ $(git status --porcelain | wc -l) -eq "0" ]; then
+  echo -e "  🟢 Git repo is clean.\n"
+else
+  echo "  🔴 Git repo dirty. Quit"
+  exit 1
+fi
 
-# navigate into the build output directory
-cd dist
+function subtree_push() {
+    git subtree push --prefix $output origin $branch
+}
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+function subtree_split() {
+    git push origin $(git subtree split --prefix $output master):gh-pages --force
+}
 
-git init
-git add -A
-git commit -m 'deploy'
+function is_in_remote() {
+    local git_command="git ls-remote --heads origin ${branch}"
+    local existed_in_remote=$($git_command)
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+    if [[ ${existed_in_remote} ]]; then
+        echo "\nremote branch $branch exists"
+        echo "\n$git_command\n"
+        echo -e "\n$existed_in_remote\n"
+        echo -e "\nupdate $branch against a rebased master branch\n"
+        subtree_split
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:aquaductape/solid-scroll-shadows.git master:gh-pages
+    else
+        echo -e "\nthere's no remote branch $branch\n"
+        subtree_push
+    fi
+}
 
-cd -
+echo -e "\nbuilding $output...\n" &&
+    npm run build &&
+    echo -e "\ntracking $output by removing it in gitingore\n"
+sed -i -- 's/dist//g' ./.gitignore &&
+    git add . &&
+    echo -e "\ncommitting $output in order to push dist subtree for $branch...\n"
+git commit -m "update $output subtree for $branch" &&
+    is_in_remote
+
+echo -e "\nremoving $output commit, reseting to previous commit\n"
+git reset --hard HEAD~1
+echo -e "\nComplete!"
