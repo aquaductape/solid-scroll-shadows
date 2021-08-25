1538ccde464854d6e86926818b1d9c680f8aa557
Caleb Taylor <aquaductape@gmail.com>
2021-08-24 20:14:37 -0700

    update dist subtree for gh-pages

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from "solid-js/web";
-import "focus-visible";
-import "@simonwep/pickr/dist/themes/nano.min.css";
-// import "./lib/colr_pickr/index.css";
-import "./styles/index.scss";
-import App from "./App";
-
-render(() => <App />, document.getElementById("root")!);
