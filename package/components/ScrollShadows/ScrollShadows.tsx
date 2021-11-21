e9340dd49dc3bacb0cb8bde5332f650b8643dc72
Caleb Taylor <aquaductape@gmail.com>
2021-11-21 00:15:14 -0800

    test

--- package/components/ScrollShadows/ScrollShadows.tsx
+++ package/components/ScrollShadows/ScrollShadows.tsx
@@ -29,51 +29,51 @@ const ScrollShadows: Component<TScrollShadows> = (props) => {
   const children = props.children as HTMLElement & ElementTemplate;
   const sentinelShadowMap: SentinelShadowMap = new Map();
   let scrollableContainer = children as HTMLElement;
-  // @ts-ignore
-  let sentinelBeforeEl: HTMLElement & ElementTemplate =
-    !disableIntersectionObserver ? (
-      <Sentinel
-        child="before"
-        direction={direction}
-        rtl={props.rtl}
-        endsMargin={endsMargin}
-      />
-    ) : null;
-  // @ts-ignore
-  let sentinelAfterEl: HTMLElement & ElementTemplate =
-    !disableIntersectionObserver ? (
-      <Sentinel
-        child="after"
-        direction={direction}
-        rtl={props.rtl}
-        endsMargin={endsMargin}
-      />
-    ) : null;
-
-  if (typeof sentinelBeforeEl === "function") {
-    // @ts-ignore
-    sentinelBeforeEl = sentinelBeforeEl();
-    // @ts-ignore
-    sentinelAfterEl = sentinelAfterEl();
-  }
-
-  if (isServer) {
-    const scrollableContainerStr = children.t;
-    const sentinelFirstElStr = sentinelBeforeEl.t;
-    const sentinelLastElStr = sentinelAfterEl.t;
-
-    const result = editHTMLStr({
-      html: scrollableContainerStr,
-      insertElementStr: sentinelFirstElStr + sentinelLastElStr,
-    });
-    children.t = result;
-  } else {
-    scrollableContainer.style.position = "relative";
-    if (!disableIntersectionObserver) {
-      scrollableContainer.appendChild(sentinelBeforeEl);
-      scrollableContainer.appendChild(sentinelAfterEl);
-    }
-  }
+  //   // @ts-ignore
+  //   let sentinelBeforeEl: HTMLElement & ElementTemplate =
+  //     !disableIntersectionObserver ? (
+  //       <Sentinel
+  //         child="before"
+  //         direction={direction}
+  //         rtl={props.rtl}
+  //         endsMargin={endsMargin}
+  //       />
+  //     ) : null;
+  //   // @ts-ignore
+  //   let sentinelAfterEl: HTMLElement & ElementTemplate =
+  //     !disableIntersectionObserver ? (
+  //       <Sentinel
+  //         child="after"
+  //         direction={direction}
+  //         rtl={props.rtl}
+  //         endsMargin={endsMargin}
+  //       />
+  //     ) : null;
+  //
+  //   if (typeof sentinelBeforeEl === "function") {
+  //     // @ts-ignore
+  //     sentinelBeforeEl = sentinelBeforeEl();
+  //     // @ts-ignore
+  //     sentinelAfterEl = sentinelAfterEl();
+  //   }
+  //
+  //   if (isServer) {
+  //     const scrollableContainerStr = children.t;
+  //     const sentinelFirstElStr = sentinelBeforeEl.t;
+  //     const sentinelLastElStr = sentinelAfterEl.t;
+  //
+  //     const result = editHTMLStr({
+  //       html: scrollableContainerStr,
+  //       insertElementStr: sentinelFirstElStr + sentinelLastElStr,
+  //     });
+  //     children.t = result;
+  //   } else {
+  //     scrollableContainer.style.position = "relative";
+  //     if (!disableIntersectionObserver) {
+  //       scrollableContainer.appendChild(sentinelBeforeEl);
+  //       scrollableContainer.appendChild(sentinelAfterEl);
+  //     }
+  //   }
 
   const state: LocalState = {
     init: true,
@@ -100,55 +100,60 @@ const ScrollShadows: Component<TScrollShadows> = (props) => {
     onScroll(state, e);
   };
 
-  const intersectionObserver: IntersectionObserver | null =
-    !disableIntersectionObserver
-      ? new IntersectionObserver((entries) => observeSentinels(state, entries))
-      : null;
-  const resizeObserver: ResizeObserver | null = new ResizeObserver((entries) =>
-    observeScrollContainer(state, entries)
-  );
+  // const intersectionObserver: IntersectionObserver | null =
+  //   !disableIntersectionObserver
+  //     ? new IntersectionObserver(
+  //         (entries) => observeSentinels(state, entries),
+  //         {
+  //           root: scrollableContainer,
+  //         }
+  //       )
+  //     : null;
+  // const resizeObserver: ResizeObserver | null = new ResizeObserver((entries) =>
+  //   observeScrollContainer(state, entries)
+  // );
 
-  onMount(() => {
-    if (!disableScrollWheel) {
-      scrollableContainer.addEventListener("wheel", _scrollHorizontally);
-    }
-
-    const setElKey = (el: HTMLElement) => {
-      if (!intersectionObserver) return {} as any;
-      return el;
-    };
-
-    sentinelShadowMap.set(setElKey(sentinelBeforeEl), {
-      type: "before",
-      el: state.shadowFirstEl,
-      visible: true,
-      sentinel: sentinelBeforeEl,
-    });
-    sentinelShadowMap.set(setElKey(sentinelAfterEl), {
-      type: "after",
-      el: state.shadowLastEl,
-      visible: false,
-      sentinel: sentinelAfterEl,
-    });
-
-    if (intersectionObserver) {
-      intersectionObserver.observe(sentinelBeforeEl);
-      intersectionObserver.observe(sentinelAfterEl);
-    } else {
-      resizeObserver.observe(scrollableContainer);
-      scrollableContainer.addEventListener("scroll", _onScroll, {
-        passive: true,
-      });
-    }
-
-    onCleanup(() => {
-      if (intersectionObserver) {
-        intersectionObserver && intersectionObserver.disconnect();
-      }
-      scrollableContainer.removeEventListener("wheel", _scrollHorizontally);
-      scrollableContainer.removeEventListener("scroll", _onScroll);
-    });
-  });
+  //   onMount(() => {
+  //     if (!disableScrollWheel) {
+  //       scrollableContainer.addEventListener("wheel", _scrollHorizontally);
+  //     }
+  //
+  //     const setElKey = (el: HTMLElement) => {
+  //       if (!intersectionObserver) return {} as any;
+  //       return el;
+  //     };
+  //
+  //     sentinelShadowMap.set(setElKey(sentinelBeforeEl), {
+  //       type: "before",
+  //       el: state.shadowFirstEl,
+  //       visible: true,
+  //       sentinel: sentinelBeforeEl,
+  //     });
+  //     sentinelShadowMap.set(setElKey(sentinelAfterEl), {
+  //       type: "after",
+  //       el: state.shadowLastEl,
+  //       visible: false,
+  //       sentinel: sentinelAfterEl,
+  //     });
+  //
+  //     if (intersectionObserver) {
+  //       intersectionObserver.observe(sentinelBeforeEl);
+  //       intersectionObserver.observe(sentinelAfterEl);
+  //     } else {
+  //       resizeObserver.observe(scrollableContainer);
+  //       scrollableContainer.addEventListener("scroll", _onScroll, {
+  //         passive: true,
+  //       });
+  //     }
+  //
+  //     onCleanup(() => {
+  //       if (intersectionObserver) {
+  //         intersectionObserver && intersectionObserver.disconnect();
+  //       }
+  //       scrollableContainer.removeEventListener("wheel", _scrollHorizontally);
+  //       scrollableContainer.removeEventListener("scroll", _onScroll);
+  //     });
+  //   });
 
   return (
     <div
@@ -157,7 +162,7 @@ const ScrollShadows: Component<TScrollShadows> = (props) => {
       classList={props.classList}
       style={props.style}
     >
-      <Shadow
+      {/* <Shadow
         shadowsClass={props.shadowsClass}
         shadowsBlockClass={props.shadowsBlockClass}
         child="before"
@@ -176,7 +181,7 @@ const ScrollShadows: Component<TScrollShadows> = (props) => {
         animation={props.animation}
         rtl={props.rtl}
         ref={state.shadowLastEl}
-      />
+      /> */}
       {scrollableContainer}
     </div>
   );
